9912c48e424ba124a3f01c04cd1fba74d24feacc
alessgb <alegbt94@gmail.com>
2024-11-20 17:16:44 +0100

    2

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Task} from "./task.model";
 
 
@@ -12,6 +12,19 @@ import {Task} from "./task.model";
 })
 export class TaskComponent {
 
-
   @Input({required:true}) task!: Task;
+  @Output() complete = new EventEmitter<string>();
+  @Output() onNewTaskAdded = new EventEmitter<boolean>();
+
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
+
+
+
+
+
+
+
+
 }
