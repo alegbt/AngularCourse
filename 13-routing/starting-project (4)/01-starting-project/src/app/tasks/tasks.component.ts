43149c3323fe838247123800d6de32c6ffc88fbf
aless <alessandrogbt94@gmail.com>
2024-12-31 16:25:11 +0100

    31 12 24

--- 13-routing/starting-project (4)/01-starting-project/src/app/tasks/tasks.component.ts
+++ 13-routing/starting-project (4)/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, DestroyRef, inject, input} from '@angular/core';
+import {Component, computed, DestroyRef, inject, input, signal} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -16,19 +16,32 @@ export class TasksComponent {
 
   userId = input.required<string>();    //qui riesce a prendere userId come param dall'url x la configurazione in app.config di withRouterConfig
 
-  // order = input<'asc' | 'desc'>();
-  order? : 'asc' | 'desc';
+  // order = input<'asc' | 'desc'>('desc');
+  order = signal<'asc' | 'desc'>('desc')
+
 
   private tasksService = inject(TasksService);
 
-  userTasks = computed(() => this.tasksService.allTasks().filter((t) => t.userId === this.userId()));
+  //listadi task che si ordinano grazie al computed in base a order() desc or asc
+  userTasks = computed(() =>
+    this.tasksService
+      .allTasks()
+      .filter((t) => t.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === 'desc'){
+          return a.id > b.id ? -1 : 1;
+        }else{
+          return a.id > b.id ? 1 : -1;
+        }
+      })
+  );
 
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
     const sub = this.activatedRoute.queryParams.subscribe({
-      next: params => this.order = params['order']
+      next: (params) => (this.order.set(params['sort'])),
     });
     this.destroyRef.onDestroy(() => sub.unsubscribe());
   }
